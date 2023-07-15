3b702fc60e6c39e148b74c5b1e09b2568b33f437
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-07-15 15:42:33 +0300

    Initial commit

---
+++ ETradeAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using ETradeAPI.Application.Abstraction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETradeAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products=_productService.Get();
+            return Ok(products);
+        }
+    }
+}
---
+++ ETradeAPI.Persistance/ServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using ETradeAPI.Application.Abstraction;
+using ETradeAPI.Persistance.Concretes;
+using ETradeAPI.Persistance.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+
+namespace ETradeAPI.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceServices(this IServiceCollection services)
+        {
+            
+            services.AddSingleton<IProductService, ProductService>();
+            services.AddDbContext<ETradeAPIDbContext>(options => options.UseNpgsql(
+               Configuration.ConnectionString));    
+        }
+    }
+}
