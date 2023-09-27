9cd1558b13cb319cc55e6340f2b2375d1a3f6ce0
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-09-27 14:06:11 +0300

    bugfic

--- ETradeAPI.API/Controllers/ProductsController.cs
+++ ETradeAPI.API/Controllers/ProductsController.cs
@@ -13,6 +13,7 @@ namespace ETradeAPI.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    //[Authorize(AuthenticationSchemes ="Admin")]
     public class ProductsController : ControllerBase
     {
 
