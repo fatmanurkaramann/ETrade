946e0f79c24fc12f09dda64473e800973f864ec8
fatmanurkaramann <fatmanur.karaman@outlook.com>
2023-10-04 12:03:23 +0300

    .

--- ETradeAPI.API/Controllers/ProductsController.cs
+++ ETradeAPI.API/Controllers/ProductsController.cs
@@ -13,7 +13,7 @@ namespace ETradeAPI.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    //[Authorize(AuthenticationSchemes ="Admin")]
+    [Authorize(AuthenticationSchemes ="Admin")]
     public class ProductsController : ControllerBase
     {
 
