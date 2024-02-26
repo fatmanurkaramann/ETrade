e77d641eb4a8e72bb47ea3872a263e4255c0fe5c
fatmanurkaramann <98432237+fatmanurkaramann@users.noreply.github.com>
2024-02-26 22:33:38 +0300

    basket controllerda getallbasket oluşturuldu

---
+++ ETradeAPI.API/Controllers/BasketsController.cs
@@ -0,0 +1,23 @@
+﻿using ETradeAPI.Application.Features.Queries.Basket.GetBasketItems;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETradeAPI.API.Controllers
+{
+    public class BasketsController : ControllerBase
+    {
+        readonly IMediator _mediatr;
+
+        public BasketsController(IMediator mediatr)
+        {
+            _mediatr = mediatr;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBasketItems(GetBasketItemsQueryRequest request)
+        {
+          List<GetBasketItemsQueryResponse> res = await _mediatr.Send(request);
+            return Ok(res);
+        }
+    }
+}
